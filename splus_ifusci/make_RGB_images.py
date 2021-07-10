5e8e3957fffd2e35658373733a0182a6f8b7f092
cebarbosa <kadu.barbosa@gmail.com>
2021-07-09 22:38:48 -0300

    Added option to control maximum percentil of RGB images.

--- splus_ifusci/make_RGB_images.py
+++ splus_ifusci/make_RGB_images.py
@@ -6,7 +6,7 @@ from astropy.stats import sigma_clipped_stats
 from PIL import Image
 import cv2
 
-def make_RGB_with_overlay(r, g, b, output, overlay=None, bb=5):
+def make_RGB_with_overlay(r, g, b, output, overlay=None, bb=10, pmax=99):
     """ Tool to produce RGB images with overlay.
 
     Parameters
@@ -20,16 +20,21 @@ def make_RGB_with_overlay(r, g, b, output, overlay=None, bb=5):
         Image to be overlay over RGB image.
     bb: float
         Smoothness parameter.
+    pp: float
+        Percentile of the maximum intensity.
     """
-    r[~np.isfinite(r)] = 0.
-    g[~np.isfinite(g)] = 0.
-    b[~np.isfinite(b)] = 0.
+    cube = [r, g, b]
+    for i, img in enumerate(cube):
+        img[~np.isfinite(img)] = 0
+        mean, median, stddev = sigma_clipped_stats(img[img != 0])
+        img = np.clip(img - median - stddev, 0, np.infty)
+        cube[i] = img
     I = (b + g + r) / 3.
     beta = np.nanmedian(I) * bb
     R = r * np.arcsinh(I / beta) / I
     G = g * np.arcsinh(I / beta) / I
     B = b * np.arcsinh(I / beta) / I
-    maxRGB = np.percentile(np.stack([R, G, B]), 99.5)
+    maxRGB = np.percentile(np.stack([R, G, B]), pmax)
     R = np.clip(255 * R / maxRGB, 0, 255).astype("uint8")
     G = np.clip(255 * G / maxRGB, 0., 255).astype("uint8")
     B = np.clip(255 * B / maxRGB, 0, 255).astype("uint8")
@@ -40,11 +45,11 @@ def make_RGB_with_overlay(r, g, b, output, overlay=None, bb=5):
         out.save(output)
         return
     mean, median, stddev = sigma_clipped_stats(overlay)
-    maxha = np.percentile(overlay, 99.5)
-    overlay = np.clip(overlay, stddev, maxha)
+    maxha = np.percentile(overlay, pmax)
+    overlay = np.clip(overlay, 2.2 * stddev, maxha)
     overlay -= overlay.min()
     overlay = (overlay / overlay.max() * 255).astype("uint8")
     hamask = np.zeros_like(RGB)
     hamask[:, :, 0] = np.rot90(overlay, 3).T
-    out = Image.fromarray(cv2.addWeighted(RGB, 0.9, hamask, 0.4, 0))
+    out = Image.fromarray(cv2.addWeighted(RGB, 0.8, hamask, 0.4, 0))
     out.save(output)
